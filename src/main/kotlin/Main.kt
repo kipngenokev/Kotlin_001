3f521e07f375d0b227cf47d52c62bad7f2caaed0
brian070069 <brianwgatundu@gmail.com>
2024-07-06 22:47:16 +0300

    Kotlin first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+fun main() {
+    println("Hello World!")
+    val a=6
+    val b=4
+     val c=b+a
+    println(c)
+
+    if (c < b ){
+        val s = 122.3
+        println("You bitch $c")
+        println("Fuck nigga $s")
+    }
+}
